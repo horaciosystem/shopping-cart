a2a38e62e7178b4c8b4f31cc61c130d9db839270
Horacio Fernandes <horaciofernandes@MacBook-Pro-de-Horacio.local>
2019-11-11 19:38:15 -0300

    Init project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route>
+            <span>Not found :(</span>
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RouterProps } from "react-router";
+
+const Home: React.FC<RouterProps> = () => (
+  <>
+    <div>Horacio</div>
+    <div>Horacio</div>
+    <div>Horacio</div>
+  </>
+);
+
+export default Home;
